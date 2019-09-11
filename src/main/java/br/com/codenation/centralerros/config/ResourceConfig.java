3041e6b195f1c05ac5ec96253641ea1dc7ea5691
Brubatera <brubatera5@gmail.com>
2019-09-11 19:28:25 -0300

    Reajuste geral do projeto

--- src/main/java/br/com/codenation/centralerros/config/ResourceConfig.java
+++ src/main/java/br/com/codenation/centralerros/config/ResourceConfig.java
@@ -1,4 +1,4 @@
-package com.challenge.invoice.config;
+package br.com.codenation.centralerros.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
