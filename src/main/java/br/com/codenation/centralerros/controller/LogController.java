82e92ab9e5cb6c3057de8e14df49cec200c4bc1d
Brubatera <brubatera5@gmail.com>
2019-09-28 09:47:07 -0300

    Paginação dos logs

--- src/main/java/br/com/codenation/centralerros/controller/LogController.java
+++ src/main/java/br/com/codenation/centralerros/controller/LogController.java
@@ -1,15 +1,15 @@
 package br.com.codenation.centralerros.controller;
 
-import br.com.codenation.centralerros.entity.LevelLog;
-import br.com.codenation.centralerros.entity.Log;
-import br.com.codenation.centralerros.entity.ServerOrigin;
+import br.com.codenation.centralerros.entity.*;
 import br.com.codenation.centralerros.services.LogService;
 import lombok.AllArgsConstructor;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -18,32 +18,44 @@ public class LogController {
 
     private LogService logService;
 
-    @GetMapping
-    public List<Log> findAllByServerOrigin(@RequestParam(value = "serverOrigin") String serverOrigin) {
-        return logService.findAllByServerOrigin(serverOrigin);
-    }
-
-    @GetMapping("/{origin}/{orderBy}")
-    public List<Log> findAllByServerOriginOrderByLevelLogDesc(@PathVariable(value = "origin") String origin, @PathVariable(value = "orderBy") String orderBy) {
-        if (orderBy.equalsIgnoreCase("orderByLevel")) {
-            return logService.findAllByServerOriginOrderByLevelLogDesc(origin);
-        }
-        //TODO: implementar query para ordenar por frequência
-        return logService.findAllByServerOrigin(origin);
-    }
-
-    @GetMapping("/search/{filter}/{value}")
-    public List<Log> findBySearchBar(@PathVariable(value = "filter") String filter, @PathVariable(value = "value") String value) {
-        if (filter.equalsIgnoreCase("level")) {
-            return logService.findByLevel(value);
-        }
-//        if (filter.equalsIgnoreCase("description")) {
-//            //TODO: implementar findByDescription
+//    @GetMapping
+//    public List<Log> findAllByServerOrigin(@RequestParam(value = "serverOrigin") String serverOrigin) {
+//        return logService.findAllByServerOrigin(serverOrigin);
+//    }
+//
+//    @GetMapping
+//    public List<Log> findAllByServerOriginOrderByLevelLogDesc(@RequestParam(value = "origin") String origin, @RequestParam(value = "orderBy") String orderBy) {
+//        if (orderBy.equalsIgnoreCase("orderByLevel")) {
+//            return logService.findAllByServerOriginOrderByLevelLogDesc(origin);
 //        }
-        if (filter.equalsIgnoreCase("origin")) {
-            return logService.findByServerOrigin(value);
-        }
-        return logService.findAll();
+//        //TODO: implementar query para ordenar por frequência
+//        return logService.findAllByServerOrigin(origin);
+//    }
+//
+//    @GetMapping("/search/{filter}/{value}")
+//    public List<Log> findBySearchBar(@PathVariable(value = "filter") String filter, @PathVariable(value = "value") String value) {
+//        if (filter.equalsIgnoreCase("level")) {
+//            return logService.findByLevel(value);
+//        }
+////        if (filter.equalsIgnoreCase("description")) {
+////            //TODO: implementar findByDescription
+////        }
+//        if (filter.equalsIgnoreCase("origin")) {
+//            return logService.findByServerOrigin(value);
+//        }
+//        return logService.findAll();
+//    }
+
+    @GetMapping
+    public Page<Log> find(@RequestParam(required = false) Optional<Long> applicationId,
+                          @RequestParam(required = false) Optional<Long> companyId,
+                          @RequestParam(required = false) Optional<ServerOrigin> origin,
+                          @RequestParam(required = false) Optional<LevelLog> levelLog,
+                          @RequestParam(required = false, defaultValue = "0") Integer page,
+                          @RequestParam(required = false, defaultValue = "20") Integer size,
+                          @RequestParam(required = false, defaultValue = "createdAt") String orderBy,
+                          @RequestParam(required = false, defaultValue = "DESC") Sort.Direction direction) {
+        return logService.find(applicationId, companyId, origin, levelLog, page, size, orderBy, direction);
     }
 
     @PutMapping("/file/{id}")
--- src/main/java/br/com/codenation/centralerros/services/LogService.java
+++ src/main/java/br/com/codenation/centralerros/services/LogService.java
@@ -1,13 +1,16 @@
 package br.com.codenation.centralerros.services;
 
-import br.com.codenation.centralerros.entity.LevelLog;
-import br.com.codenation.centralerros.entity.Log;
-import br.com.codenation.centralerros.entity.ServerOrigin;
+import br.com.codenation.centralerros.entity.*;
 import br.com.codenation.centralerros.repository.LogRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -54,5 +57,15 @@ public class LogService {
 
     public List<Log> findLogsByApplication(Long companyId, Long userId) {
         return logRepository.findLogsByApplicationAndCompanyAndUser(companyId, userId);
+    }
+
+    public Page<Log> find(Optional<Long> applicationId, Optional<Long> companyId, Optional<ServerOrigin> origin, Optional<LevelLog> levelLog, Integer page, Integer size, String orderBy, Sort.Direction direction) {
+        Log log = Log.builder()
+                .application(applicationId.map(id -> Application.builder().id(id).build()).orElse(null))
+                .company(companyId.map(id -> Company.builder().id(id).build()).orElse(null))
+                .serverOrigin(origin.orElse(null))
+                .levelLog(levelLog.orElse(null))
+                .build();
+        return logRepository.findAll(Example.of(log), PageRequest.of(page, size, Sort.by(direction, orderBy)));
     }
 }
